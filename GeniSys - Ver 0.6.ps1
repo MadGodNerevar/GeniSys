8bed3b5c82618b3b0ea78d553f5dfe898b567f8d
MadGodNerevar <34100671+MadGodNerevar@users.noreply.github.com>
2021-04-25 19:25:49 +0100

    Update GeniSys - Ver 0.6.ps1

--- GeniSys - Ver 0.6.ps1
+++ GeniSys - Ver 0.6.ps1
@@ -1,23 +1,23 @@
 ﻿mode 300
 
 Echo '
- 0000000000000000   000000000000000     0000        00    000000000000000    000000000000000    00           00    000000000000000
-00000000000000000  00000000000000000  0000000       000  00000000000000000  00000000000000000   000         000   00000000000000000
-000                000                000  000      000         000         000                  000       000    000
-000                000                000   000     000         000          000                  000     000      000
-000      0000000   0000000000000000   000    000    000         000           000000000000         000000000        000000000000
-000      00000000  0000000000000000   000     000   000         000            000000000000          00000           000000000000
-000           000  000                000      000  000         000                      000          000                      000
-000           000  000                000       000 000         000                       000         000                       000
-00000000000000000  00000000000000000  000        000000  00000000000000000  00000000000000000         000         00000000000000000
- 0000000000000000   000000000000000    00         0000    000000000000000    000000000000000          000          000000000000000
+ 0000000000000000   000000000000000     0000        00    000000000000000    00000000000000   00           00   00000000000000
+00000000000000000  00000000000000000  0000000       000  00000000000000000  0000000000000000  000         000  0000000000000000
+000                000                000  000      000         000         000                000       000   000
+000                000                000   000     000         000          000                000     000     000
+000      0000000   0000000000000000   000    000    000         000           000000000000       000000000       000000000000
+000      00000000  0000000000000000   000     000   000         000            000000000000        00000          000000000000
+000           000  000                000      000  000         000                      000        000                     000
+000           000  000                000       000 000         000                       000       000                      000
+00000000000000000  00000000000000000  000        000000  00000000000000000  00000000000000000       000        00000000000000000
+ 0000000000000000   000000000000000    00         0000    000000000000000    000000000000000        000         000000000000000
   
 ####################################################################################################################################
 ####################################################################################################################################'
 
-Echo ('Welcome to GeniSys')
+Echo ('Welcome to GeniSys, Installer Kit')
 
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 2
 
 # Get the ID and security principal of the current user account
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
@@ -55,19 +55,28 @@ else {
 # Code for elevation pop here
 
 #Need to turn this into an if/else statement and to ask if its required in the first place
-#Asks for the IP address accociated with DNS and sets the DNS to that address
-$DNS = Read-Host "Enter DNS Address"
-Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' , 'Ethernet 0' , 'Ethernet 1' , 'Ethernet 2' , 'Ethernet 3' , 'Ethernet 4' -ServerAddresses ("$DNS") -ErrorAction SilentlyContinue
+$useranswer = Read-Host "Do You Need to Enter DNS Address"
 
-#Clears the Cache
-Clear-DnsClientCache
+if ($useranswer -eq "y", "Y", "Yes") {
+
+    #Asks for the IP address accociated with DNS and sets the DNS to that address
+    $DNS = Read-Host "Enter DNS Address"
+    Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' , 'Ethernet 0' , 'Ethernet 1' , 'Ethernet 2' , 'Ethernet 3' , 'Ethernet 4' -ServerAddresses ("$DNS") -      ErrorAction SilentlyContinue
+
+     #Clears the Cache 
+     Clear-DnsClientCache
+}
+else {
+
+     Write-Out "Carrying on With Install"
+}
 
 #Asks for user input to enter in the Domain name, Domain Admin and the name of the server
 $UserDomain = Read-Host "Enter Domain"
 $DomainAdmin = Read-Host "Enter Domain Admin"
 $Server = Read-Host "Enter Server Name"
 
-#you can set this as as a variable or just have a simple user - more on this later
+#you can set this as as a variable or just have a simple user
 $Username = 'Term'
 
 #I mean this just adds the computer to the domain using the credentials popped in by user input 
@@ -78,6 +87,7 @@ Add-LocalGroupMember -Group "Administrators" -Member "$UserDomain\Term"
 
 #Asks for username and gives full path fo HKLM
 $Password = Read-Host "Enter a Password for Term if Necessary"
+
 #$Username = $env:UserName #think this is only useful if you want the current signed in user to be the one that is used
 $RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\winlogon"
 
@@ -97,12 +107,54 @@ Copy-Item "\\$Server\Quadranet\Quadranet\EposV5\App\EposV5\" -Destination "C:\Qu
 #Get path to removeable drive and software folder
 
 #Install TeamViewer
+Start-Process -FilePath "TeamViewer.exe"
+
+Start-Sleep -Seconds 20
 
 #Select and install Printer Driver then rename
+#need to find a way to list the printers then take user input and pass to if else statements
+
+$selectedprinter = $selecteddriver
+if ($selectedprinter -eq "RP-320") {
+
+    Start-Process -FilePath "Driver.exe" 
+    Write-Output "$selectedprinter will be renamed."
+}
+elseif ($selectedprinter -eq "RP-600") {
+
+    Start-Process -FilePath "Driver.exe"
+    Write-Output "$selectedprinter will be renamed."
+}
+elseif ($selectedprinter -eq "RP-700") {
+
+    Start-Process -FilePath "Driver.exe"
+    Write-Output "$selectedprinter will be renamed."
+}
+
+$printername = $selectedprinter
+
+Start-Sleep -Seconds 60 #curious to see if i can make this wait for printer installation then continue
 
 Rename-Printer -Name "$printername" -NewName "Local"
 
-#Select and Install OPOS Driver
+#Select and Install OPOS Driver this will need to vary based upon the terminal
+if ($OPOSDriver -eq "need to get drivers" "SP-1030") {
+
+    Start-Process -FilePath "Driver.exe" 
+    Write-Output "$OPOSDriver will need to be set to ".
+}
+elseif ($OPOSDriver -eq "need to get drivers" "SP-1060") {
+
+    Start-Process -FilePath "Driver.exe"
+    Write-Output "$OPOSDriver will need to be set to ".
+}
+elseif ($OPOSDriver -eq "need to get drivers" "Audrey") {
+
+    Start-Process -FilePath "Driver.exe"
+    Write-Output "$OPOSDriver will need to be set to KS-A".
+}
+
+Start-Sleep -Seconds 20
 
 #I mean self explanatory it restarts the PC
 Restart-Computer
